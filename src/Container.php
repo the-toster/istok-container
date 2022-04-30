ea74e1819946072806b188a1dd8d4fa9707b4a16
programmer <internetov.ru@gmail.com>
2022-04-30 19:11:06 +0300

    basic resolving logic

--- src/Container.php
+++ src/Container.php
@@ -4,11 +4,31 @@ declare(strict_types=1);
 
 namespace TheToster\Container;
 
+use Closure;
 use Psr\Container\ContainerInterface;
+
+use ReflectionClass;
+use ReflectionFunction;
+use ReflectionIntersectionType;
+
+use ReflectionNamedType;
+use ReflectionParameter;
+use ReflectionType;
+use ReflectionUnionType;
+
+use RuntimeException;
+
+use Throwable;
+
+use function Reflection;
 
 final class Container implements ContainerInterface
 {
+    /** @var array<string,Closure|string> */
     private array $items = [];
+
+    /** @var array<string,array<string,Closure>> */
+    private array $params = [];
 
     public function get(string $id): mixed
     {
@@ -20,41 +40,16 @@ final class Container implements ContainerInterface
 
         try {
             return $this->resolve($entity);
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             throw new NotResolvable($id, 0, $e);
         }
     }
 
-    private function resolve(mixed $entity): mixed
+    public function call(Closure $closure): mixed
     {
-        if ($entity instanceof \Closure) {
-            return $this->call($entity);
-        }
+        $reflection = new ReflectionFunction($closure);
 
-        if (class_exists($entity)) {
-            return $this->construct($entity);
-        }
-    }
-
-    private function construct(string $className): object
-    {
-        $reflection = new \ReflectionClass($className);
-        $constructor = $reflection->getConstructor();
-
-        if (!$constructor->isPublic()) {
-            throw new NotResolvable();
-        }
-
-        $args = $this->resolveArguments($constructor->getParameters());
-
-        return new $className(...$args);
-    }
-
-    public function call(\Closure $closure): mixed
-    {
-        $reflection = new \ReflectionFunction($closure);
-
-        $args = $this->resolveArguments($reflection->getParameters());
+        $args = $this->resolveArguments($reflection->getParameters(), null);
 
         return $closure(...$args);
     }
@@ -64,43 +59,86 @@ final class Container implements ContainerInterface
         return isset($this->items[$id]);
     }
 
-    public function set(string $id, mixed $def): void
+    public function set(string $id, Closure|string $def): void
     {
         $this->items[$id] = $def;
     }
 
-    private function resolveType(\ReflectionIntersectionType|\ReflectionUnionType|\ReflectionNamedType $type): mixed
+    private function resolve(mixed $entity): mixed
     {
-        if ($type->allowsNull()) {
-            return null;
+        if ($entity instanceof Closure) {
+            return $this->call($entity);
         }
-        return $this->get((string)$type);
+
+        if (is_string($entity) && class_exists($entity)) {
+            return $this->construct($entity);
+        }
+
+        throw new NotResolvable();
     }
 
-    private function resolveByName(string $name)
+    /**
+     * @template T
+     * @param class-string<T> $className
+     * @return T
+     */
+    private function construct(string $className): object
     {
-        throw new \RuntimeException('unimplemented');
+        $reflection = new ReflectionClass($className);
+        $constructor = $reflection->getConstructor();
+
+        if ($constructor && !$constructor->isPublic()) {
+            throw new NotResolvable();
+        }
+
+        $args = $this->resolveArguments($constructor?->getParameters() ?? [], $className);
+
+        /** @psalm-suppress MixedMethodCall */
+        return new $className(...$args);
     }
 
-    /** @param \ReflectionParameter[] $parameters */
-    private function resolveArguments(array $parameters): array
+    /** @param ReflectionParameter[] $parameters */
+    private function resolveArguments(array $parameters, ?string $forId): array
     {
         $arguments = [];
         foreach ($parameters as $parameter) {
+            /** @psalm-suppress MixedAssignment */
+            [$resolved, $value] = $this->resolveByName($parameter->getName(), $forId);
+
+            if ($resolved) {
+                /** @psalm-suppress MixedAssignment */
+                $arguments[] = $value;
+                continue;
+            }
+
             if ($parameter->isOptional()) {
+                /** @psalm-suppress MixedAssignment */
                 $arguments[] = $parameter->getDefaultValue();
                 continue;
             }
 
             $type = $parameter->getType();
-            if ($type) {
-                $arguments[] = $this->resolveType($type);
+            if ($type instanceof ReflectionNamedType) {
+                /** @psalm-suppress MixedAssignment */
+                $arguments[] = $this->get((string)$type);
                 continue;
             }
 
-            $arguments[] = $this->resolveByName($parameter->name);
+            throw new NotResolvable();
         }
 
         return $arguments;
     }
+
+    /** @return array{0: bool, 1:mixed} */
+    private function resolveByName(string $name, ?string $id): array
+    {
+        $resolver = $this->params[$id][$name] ?? null;
+        if (!$resolver) {
+            return [false, null];
+        }
+
+        return [true, $this->call($resolver)];
+    }
+
 }
